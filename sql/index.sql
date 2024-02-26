9e636aba7143acde7153365bda92ea6deafbb782
Ususi(Даниил) <99796823+Muhametgaleev@users.noreply.github.com>
2024-02-26 17:35:02 +0300

    Update index.sql

--- sql/index.sql
+++ sql/index.sql
@@ -1,5 +1,5 @@
 create index action_to_result_id on action_to_result using hash (result_id);
-create index action_id_idx on action using btree (status_id);
+create index action_id_idx on action using hash (status_id);
 
 --чаще всего мы будем искать задание по его статусу (например, сюжет или побочное)
 --так же мы будем часто получать результат и нам нужно его эффективно искать
